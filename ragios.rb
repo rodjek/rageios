ba9b346eae3014e21f60061d5c7d9f4e1bf76f56
Tim Sharpe <tim@picaroon.bridge.anchor.net.au>
2009-12-10 11:49:18 +1100

    Initial commit

---
+++ ragios.rb
@@ -0,0 +1,48 @@
+# Yes, I know having this in 1 giant file is all sorts of not nice.  I'm not
+# that far through the book yet, mixins & modules are still a couple of
+# chapters away
+
+require 'rubygems'
+require 'resque'
+require 'open3'
+
+module Ragios
+    module Check
+        include Ragios
+
+        class Ragios::Check::Exec
+            @queue = :check
+
+            def self.perform(host, service, executable, *args)
+                args_string = args.join(' ')
+                stdin, stdout, stderr = Open3.popen3("#{executable} #{args_string}")
+
+                Resque.enqueue(Ragios::Reaper::ServiceCheck, host, service, $?, stdout.read())
+            end
+        end
+    end
+
+    module Reaper
+        include Ragios
+
+        class Ragios::Reaper::HostCheck
+            @queue = :reaper
+
+            def self.perform(host, status, message)
+                File.open('/tmp/nagios_foo', 'a') { |fd|
+                    fd.puts "#{host};#{status};#{foo}"
+                }
+            end
+        end
+
+        class Ragios::Reaper::ServiceCheck
+            @queue = :reaper
+
+            def self.perform(host, service, status, message)
+                File.open('/tmp/nagios_foo', 'a') { |fd|
+                    fd.puts "#{host};#{service};#{status};#{message}"
+                }
+            end
+        end
+    end
+end
